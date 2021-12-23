1f9f6e040b5f70e91fd54f5c90f0d618ef13d332
chenfu <https://chenfushi@github.com>
2021-12-23 10:40:58 +0000

    Revert "remove QC_scripts"
    This reverts commit 3b8dd37d30671b1c30aa08239261902102d03a72.

---
+++ QC_scripts/run_qc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash --login
+#$ -pe smp.pe 16
+#$ -j y
+#$ -o /mnt/jw01-aruk-home01/projects/psa_functional_genomics/master_ATAC_ChIP_analyzer/ATAC_ChIP_pipeline/logs
+
+INDEX=$((SGE_TASK_ID-1))
+# CD to directory
+cd /mnt/jw01-aruk-home01/projects/psa_functional_genomics/master_ATAC_ChIP_analyzer/ATAC_ChIP_pipeline
+
+# Inform the app how many cores we requested for our job. The app can use this many cores.
+# The special $NSLOTS keyword is automatically set to the number used on the -pe line above.
+export OMP_NUM_THREADS=$NSLOTS
+
+# activate all neeeded modules and packages
+source activate personal_software
+
+python ./QC_scripts/atac_qc.py > QC_scripts/qc_results.txt
+
